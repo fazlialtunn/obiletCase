d040c13e4a46f4b0a0952d35ff3466696b0051e8
Fazlı Altun <altunfazl@gmail.com>
2024-08-21 13:29:50 +0300

    APICaller protocol is added New error handling mechanism

--- obiletCase/Network/APICaller.swift
+++ obiletCase/Network/APICaller.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
+protocol APIProtocol {
+    func fetchData(completion: @escaping (Result<[ProductModel], FetchDataError>) -> Void)
+}
+
 /// A class responsible for making network requests to fetch product data.
-class APICaller {
+class APICaller: APIProtocol {
     private var session: URLSession
     
     init(session: URLSession = .shared) {
@@ -18,10 +22,9 @@ class APICaller {
     /// Fetches product data from the API.
     /// - Parameter completion: A closure that gets called with the result of the network request.
     ///   It returns either an array of ProductModel on success or an Error on failure.
-    func fetchData(completion: @escaping (Result<[ProductModel], Error>) -> Void) {
+    func fetchData(completion: @escaping (Result<[ProductModel], FetchDataError>) -> Void) {
         guard let url = URL(string: ServiceConstants.serviceEndpoint()) else {
-            let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
-            completion(.failure(error))
+            completion(.failure(.invalidURL))
             return
         }
         
@@ -30,26 +33,23 @@ class APICaller {
         let task = session.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("Error fetching data: \(error.localizedDescription)")
-                completion(.failure(error))
+                completion(.failure(.decodingError))
                 return
             }
             
             guard let data = data else {
-                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])
-                completion(.failure(error))
+                completion(.failure(.noData))
                 return
             }
             
             do {
                 let products = try JSONDecoder().decode([ProductModel].self, from: data)
                 completion(.success(products))
-            } catch let decodingError {
-                print("Decoding error: \(decodingError.localizedDescription)")
-                completion(.failure(decodingError))
+            } catch {
+                print("Decoding error: \(error.localizedDescription)")
+                completion(.failure(.decodingError))
             }
         }
-        
         task.resume()
     }
-
 }
---
+++ obiletCase/Network/FetchDataError.swift
@@ -0,0 +1,12 @@
+//
+//  FetchDataError.swift
+//  obiletCase
+//
+//  Created by Fazlı Altun on 21.08.2024.
+//
+
+enum FetchDataError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
